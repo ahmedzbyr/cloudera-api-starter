ffd2f2914f87398bb8ac6c4cdfdfc4b076b3a0aa
Zubair AHMED <ahmedzbyr@gmail.com>
2018-03-16 18:33:49 +0100

    adding basic cdh - WIP

---
+++ cloudera_deploy/cdh_deploy.py
@@ -0,0 +1,211 @@
+
+#
+# Importing required modules.
+#
+import time, sys, yaml, logging, argparse
+from cm_api.api_client import ApiResource, ApiException
+from cm_api.endpoints.services import ApiServiceSetupInfo
+import os
+from subprocess import call
+
+
+class ClouderaManagerSetup():
+
+
+    def __init__(self, config, trial_version=False, license_information=None):
+        self.config = config
+        self.trial = trial_version
+        self.license_information = license_information
+        self.cluster = None
+        self._api_resource = None
+        self._cloudera_manager_handle = None
+
+    @property
+    def api_resource(self):
+        if self._api_resource is None:
+            self._api_resource = ApiResource(self.config['cm']['host'],
+                                             self.config['cm']['port'],
+                                             self.config['cm']['username'],
+                                             self.config['cm']['password'],
+                                             self.config['cm']['tls'],
+                                 version=self.config['cm']['api-version'])
+
+        return self._api_resource
+
+
+    @property
+    def cloudera_manager_handle(self):
+        if self._cloudera_manager_handle is None:
+            self._cloudera_manager_handle = self.api_resource.get_cloudera_manager()
+        return  self._cloudera_manager_handle
+
+
+    def enable_trial_license_for_cm(self):
+
+        try:
+            cloudera_license = self.cloudera_manager_handle.get_license()
+            logging.debug("Currently Lic Information: " + str(cloudera_license))
+        except ApiException:
+            self.cloudera_manager_handle.begin_trial()
+
+
+    def host_installation(self):
+        """
+            Host installation.
+            https://cloudera.github.io/cm_api/epydoc/5.10.0/cm_api.endpoints.cms.ClouderaManager-class.html#host_install
+        """
+        logging.info("Installing HOSTs.")
+        cmd = self.cloudera_manager_handle.host_install(self.config['cm_host_installation']['host_username'],
+                               self.config['cluster']['hosts'],
+                               ssh_port=self.config['cm_host_installation']['host_ssh_port'],
+                               private_key=self.config['cm_host_installation']['host_private_key'],
+                               parallel_install_count=self.config['cm_host_installation']['host_parallel_install_count'],
+                               cm_repo_url=self.config['cm_host_installation']['host_cm_repo_url'],
+                               gpg_key_custom_url=self.config['cm_host_installation']['host_cm_repo_gpg_key_custom_url'],
+                               java_install_strategy=self.config['cm_host_installation']['host_java_install_strategy'],
+                               unlimited_jce=self.config['cm_host_installation']['host_unlimited_jce_policy'])
+
+        #
+        # Check command to complete.
+        #
+        if not cmd.wait().success:
+            logging.info("Command `host_install` Failed. {0}".format(cmd.resultMessage))
+            if (cmd.resultMessage is not None and
+                        'There is already a pending command on this entity' in cmd.resultMessage):
+                raise Exception("HOST INSTALLATION FAILED.")
+
+
+
+    def init_cluster(self):
+        try:
+            self.cluster = self.api_resource.get_cluster(self.config['cluster']['name'])
+            return self.cluster
+
+        except ApiException:
+            cluster = self.api_resource.create_cluster(config['cluster']['name'],
+                                                    config['cluster']['version'],
+                                                    config['cluster']['fullVersion'])
+
+
+
+        cluster_hosts = []
+        #
+        # Picking up all the nodes from the yaml configuration.
+        #
+        for host_in_cluster in cluster.list_hosts():
+            cluster_hosts.append(host_in_cluster)
+
+        hosts = []
+
+        #
+        # Create a host list, make sure we dont have duplicates.
+        #
+        for host in config['cluster']['hosts']:
+            if host not in cluster_hosts:
+                hosts.append(host)
+
+        #
+        # Adding all hosts to the cluster.
+        #
+        cluster.add_hosts(hosts)
+        return self.cluster
+
+
+    def deploy_cloudera_management_services(self):
+
+        """
+            Deploy management services, below are the list of services created.
+                - Activity Monitor
+                - Alert Publisher
+                - Event Server
+                - Host Monitor
+                - Reports Manager
+                - Service Monitor
+        :return:
+        """
+        logging.info("Deploying Management Server Services")
+        try:
+            #
+            # try to get the current service if already present and running.
+            #
+            mgmt_service = self.cloudera_manager_handle.get_service()
+            if mgmt_service.serviceState == "STARTED":
+                logging.info("MGMT service already created on the server")
+                return
+
+
+        except ApiException:
+            #
+            # If not then we create a mgmt service.
+            #
+            logging.info("Creating MGMT server")
+            mgmt_service = self.cloudera_manager_handle.create_mgmt_service(ApiServiceSetupInfo())
+
+        #
+        # Now add all services to the if not already present
+        #
+        for role in config['services']['MGMT']['roles']:
+            if not len(mgmt_service.get_roles_by_type(role['group'])) > 0:
+                logging.info("Creating role for {0}".format(role['group']))
+                mgmt_service.create_role('{0}-1'.format(role['group']), role['group'], role['hosts'][0])
+
+        #
+        # Update configuration for each service.
+        #
+        for role in config['services']['MGMT']['roles']:
+            role_group = mgmt_service.get_role_config_group('mgmt-{0}-BASE'.format(role['group']))
+            logging.info(role_group)
+            #
+            # Update the group's configuration.
+            # [https://cloudera.github.io/cm_api/epydoc/5.10.0/cm_api.endpoints.role_config_groups.ApiRoleConfigGroup-class.html#update_config]
+            #
+            role_group.update_config(role.get('config', {}))
+
+        #
+        # Start mgmt services.
+        #
+        mgmt_service.start().wait()
+
+        #
+        # Wait and restart mgmt service just to make sure.
+        #
+        logging.info("Waiting for MGMT service to restart. !!!")
+
+        #
+        # Check for mgmt service, if started, else bail out.
+        #
+        if self.cloudera_manager_handle.get_service().serviceState == 'STARTED':
+            logging.info("Management Services started")
+        else:
+            logging.ERROR("[MGMT] Cloudera Management services didn't start up properly")
+
+
+
+if __name__ == '__main__':
+
+    command_line_opt = argparse.ArgumentParser("Setting up cloudera cluster using automation script.")
+    command_line_opt.add_argument('-f', '--file', help='YAML configuration file for auto deployment.', required=True)
+    command_line_opt.add_argument('-d', '--debug', help='Running debug mode.', action="store_true")
+    command_line_opt.add_argument('-v', '--version', help="Show script version", action="version",
+                                  version="CDH Auto Deploy 0.1.0")
+
+    args = command_line_opt.parse_args()
+    file_name = args.file
+
+    if args.debug:
+        logging.basicConfig(level=logging.DEBUG)
+    else:
+        logging.basicConfig(level=logging.INFO)
+
+    if not os.path.isfile(file_name):
+        logging.error("File Not Present")
+        command_line_opt.print_help()
+
+    try:
+        with open(file_name, 'r') as cluster_yaml:
+            config = yaml.load(cluster_yaml)
+
+        print config
+
+    except IOError as e:
+        logging.error("ERROR {0}. EXIT NOW :( !!!!".format(e))
